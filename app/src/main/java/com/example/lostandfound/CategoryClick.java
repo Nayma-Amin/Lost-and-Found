7486d380354c890a23e588effec14b149ca5ac4b
naymaaminnishy <naymaaminnishy06@gmail.com>
2025-05-17 17:09:48 +0600

    Initial commit Initial push

--- app/src/main/java/com/example/lostandfound/CategoryClick.java
+++ app/src/main/java/com/example/lostandfound/CategoryClick.java
@@ -8,14 +8,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class CategoryClick extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_act), (v, insets) -> {
+        setContentView(R.layout.activity_category);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.category), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
--- app/src/main/java/com/example/lostandfound/category_click.java
+++
@@ -1,4 +0,0 @@
-package com.example.lostandfound;
-
-public class category_click {
-}
